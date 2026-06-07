6672481913ea8a7961cf6ce81ff695be675f734c
Gary White <glw494@gmail.com>
2026-06-06 18:04:34 -0700

    Load ROM data to into memory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() -> Result<(), std::io::Error> {
+    let path: &str = "roms/Legend of Zelda, The - Link's Awakening (USA, Europe).gb";
+    let rom: Vec<u8> = fs::read(path)?;
+    println!("Loaded {} bytes", rom.len());
+    Ok(())
 }
